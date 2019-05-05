a42ba9c3d2f0ef11a6e4a285bde24e2853ab3705
itwx <942774363@qq.com>
2019-05-05 21:53:08 +0800

    测试一下github

--- src/cn/itwx/leetcode61_70/Leetcode61.java
+++ src/cn/itwx/leetcode61_70/Leetcode61.java
@@ -3,7 +3,7 @@ package cn.itwx.leetcode61_70;
 public class Leetcode61 {
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("2");
         System.out.println("2");
     }
 }
