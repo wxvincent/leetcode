9267d98355b5aecedc908b3658a31e442612e577
itwx <942774363@qq.com>
2019-05-05 17:05:56 +0800

    5/5下午完成

---
+++ src/cn/itwx/leetcode51_60/Leetcode58.java
@@ -0,0 +1,15 @@
+package cn.itwx.leetcode51_60;
+
+public class Leetcode58 {
+
+    public int lengthOfLastWord(String s) {
+        if (s == null) return 0;
+
+        String[] split = s.split(" ");
+        if (split.length == 0) return 0;
+
+        return split[split.length - 1].length();
+    }
+
+
+}
---
+++ src/cn/itwx/leetcode51_60/Leetcode59.java
@@ -0,0 +1,43 @@
+package cn.itwx.leetcode51_60;
+
+public class Leetcode59 {
+
+    public int[][] generateMatrix(int n) {
+
+        int[][] res = new int[n][n];
+
+        int start = 0;
+        int[] index = {1};
+        while (2 * start < n) {
+            generate(res, start, index);
+            start++;
+        }
+        return res;
+    }
+
+    private void generate(int[][] res, int start, int[] index) {
+        int endX = res.length - 1 - start, endY = res.length - 1 - start;
+        for (int i = start; i <= endX; i++) {
+            res[start][i] = index[0];
+            index[0]++;
+        }
+        if (start < endY) {
+            for (int i = start + 1; i <= endY; i++) {
+                res[i][endX] = index[0];
+                index[0]++;
+            }
+        }
+        if (start < endY && start < endX) {
+            for (int i = endX - 1; i >= start; i--) {
+                res[endY][i] = index[0];
+                index[0]++;
+            }
+        }
+        if (start < endY - 1 && start < endX) {
+            for (int i = endY - 1; i >= start + 1; i--) {
+                res[i][start] = index[0];
+                index[0]++;
+            }
+        }
+    }
+}
