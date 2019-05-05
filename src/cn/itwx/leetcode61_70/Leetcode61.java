b53a7d1f155baafc698c993bb4e45523638c5a7f
itwx <942774363@qq.com>
2019-05-05 22:09:28 +0800

    测试一下github

--- src/cn/itwx/leetcode61_70/Leetcode61.java
+++ src/cn/itwx/leetcode61_70/Leetcode61.java
@@ -1,8 +1,12 @@
 package cn.itwx.leetcode61_70;
 
+import java.util.Scanner;
+
+
 public class Leetcode61 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("12345");
         System.out.println("22222");
     }
