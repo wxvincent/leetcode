4ae14ad63dfd0ad331d1660f8222afc351df48ab
itwx <942774363@qq.com>
2019-05-10 17:49:20 +0800

    5/10下午提交

---
+++ src/cn/itwx/leetcode71_80/Leetcode78.java
@@ -0,0 +1,25 @@
+package cn.itwx.leetcode71_80;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode78 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        dfs(res, temp, nums, 0);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int start) {
+        res.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            dfs(res, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+}
---
+++ src/cn/itwx/leetcode71_80/Leetcode79.java
@@ -0,0 +1,40 @@
+package cn.itwx.leetcode71_80;
+
+public class Leetcode79 {
+
+    public boolean exist(char[][] board, String word) {
+        if (board == null || word == null || word.length() == 0) return false;
+
+        int[][] flag = new int[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (isExist(board, flag, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+    private boolean isExist(char[][] board, int[][] flag, String word, int i, int j, int index) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length ||
+                board[i][j] != word.charAt(index) || flag[i][j] == 1)
+            return false;
+
+        if (index == word.length() - 1) return true;
+
+        flag[i][j] = 1;
+        if (isExist(board, flag, word, i + 1, j, index + 1) ||
+                isExist(board, flag, word, i - 1, j, index + 1) ||
+                isExist(board, flag, word, i, j + 1, index + 1) ||
+                isExist(board, flag, word, i, j - 1, index + 1)) {
+            return true;
+        }
+        flag[i][j] = 0;
+
+        return false;
+    }
+}
