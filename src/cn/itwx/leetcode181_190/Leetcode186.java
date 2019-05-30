483e90d4d3ef26fe1a9b64a92f5bef9b972be4ef
itwx <942774363@qq.com>
2019-05-30 11:20:27 +0800

    5/30上午提交

---
+++ src/cn/itwx/leetcode181_190/Leetcode186.java
@@ -0,0 +1,26 @@
+package cn.itwx.leetcode181_190;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Leetcode186 {
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null || s.length() == 0) return res;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < s.length() - 9; i++) {
+            String index = s.substring(i, i + 10);
+            if (!set.contains(index)) {
+                set.add(index);
+            } else {
+                if (!res.contains(index)) {
+                    res.add(index);
+                }
+            }
+        }
+        return res;
+    }
+}
